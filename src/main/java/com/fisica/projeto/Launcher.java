11761339258334b040264fcdcf991fee0e077d03
Emanuel Rodrigues <emanuelrodrigues112358@gmail.com>
2025-07-16 20:21:37 -0300

    feat: implementando a lógica para ter um executável

---
+++ src/main/java/com/fisica/projeto/Launcher.java
@@ -0,0 +1,12 @@
+package com.fisica.projeto;
+
+/**
+ * Esta é uma classe lançadora para contornar um problema comum do JavaFX
+ * ao criar um JAR executável. Ela serve como o ponto de entrada principal
+ * que simplesmente chama o main da aplicação real.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
